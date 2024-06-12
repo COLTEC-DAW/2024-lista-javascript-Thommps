f04c75bd8d48df1fcafd3902170e34fb6f896262
Thommps <arcanjodaviarcanjo@gmail.com>
2024-06-12 16:17:07 -0300

    ex7

---
+++ .github/contaChar.js
@@ -0,0 +1,11 @@
+function contaChar(frase, c) {
+  let contador = 0;
+    
+  for (let i = 0; i < frase.length; i++) {
+    if (frase[i] === c) {
+      contador++;
+    }
+  }
+    
+  return contador;
+}
